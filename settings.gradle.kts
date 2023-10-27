ae6eb3a18b6435734e9fadcb85a9c2df3c3ddbc6
Hassan <hassan@gmail.com>
2023-10-27 20:30:54 +0430

    initial commit

---
+++ settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Xeiomi Calculator2"
+include(":app")
+ 
